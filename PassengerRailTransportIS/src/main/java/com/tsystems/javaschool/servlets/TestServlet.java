5eb115d8b602fb81dbd83973bf01ee4502edd135
anton_18 <anton.leonov18@gmail.com>
2015-11-10 18:22:13 +0300

    Add servlet, that shows Hello message

---
+++ PassengerRailTransportIS/src/main/java/com/tsystems/javaschool/servlets/TestServlet.java
@@ -0,0 +1,35 @@
+package com.tsystems.javaschool.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Class for showing simple string to test environment
+ * @author Anton
+ * @version 0.0.1
+ *
+ */
+
+public class TestServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = 8810213357636837414L;
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<body>");
+		out.println("<h2>Hello from Simple Servlet</h2>");
+		out.println("</body>");
+		out.println("</html>");
+		
+		resp.setStatus(HttpServletResponse.SC_OK);
+		resp.setContentType("text/html;charset=utf-8");
+	}
+	
+}
